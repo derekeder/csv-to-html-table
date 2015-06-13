d452634f3888782856b35a6ec992964dd5da2a4c
Derek Eder <derek.eder@gmail.com>
2015-06-13 14:22:23 -0500

    initial commit - loads a csv and makes an HTML table

---
+++ README.md
@@ -0,0 +1,27 @@
+# CSV to HTML Table
+
+## Running locally
+
+``` bash
+git clone git@github.com:derekeder/csv-to-html-table.git
+cd csv-to-html-table
+python -m SimpleHTTPServer
+```
+
+navigate to http://localhost:8000/
+
+## Errors / Bugs
+
+If something is not behaving intuitively, it is a bug, and should be reported.
+Report it here: https://github.com/datamade/csv-to-html-table/issues
+
+## Note on Patches/Pull Requests
+ 
+* Fork the project.
+* Make your feature addition or bug fix.
+* Commit, do not mess with rakefile, version, or history.
+* Send a pull request. Bonus points for topic branches.
+
+## Copyright
+
+Copyright (c) 2015 Derek Eder. Released under the [MIT License](https://github.com/derekeder/csv-to-html-table/blob/master/LICENSE).
---
+++ js/csv_to_html_table.js
@@ -0,0 +1,33 @@
+function init_table(csv_path, el) {
+
+  $("#" + el).html("<table class='table table-striped' id='my-table'></table>");
+
+  $.when($.get(csv_path)).then(
+    function(data){
+      var csv_data = $.csv.toArrays(data);
+      console.log(csv_data)
+
+      var table_head = "<thead><tr>";
+
+      for (head_id = 0; head_id < csv_data[0].length; head_id++) { 
+        table_head += "<th>" + csv_data[0][head_id] + "</th>";
+      }
+
+      table_head += "</tr></thead>";
+      $('#my-table').append(table_head);
+      $('#my-table').append("<tbody></tbody>");
+
+      for (row_id = 1; row_id < csv_data.length; row_id++) { 
+        var row_html = "<tr>";
+
+          for (col_id = 0; col_id < csv_data[row_id].length; col_id++) { 
+            row_html += "<td>" + csv_data[row_id][col_id] + "</td>";
+          }
+          
+        row_html += "</tr>";
+        $('#my-table tbody').append(row_html);
+      }
+
+      $("#my-table").DataTable();
+    });
+}
