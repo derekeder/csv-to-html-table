ecc3be25d9522c315fb8c767f5ff4ccbd74f1b0b
Niklas <nb@niklasbarning.de>
2022-11-05 09:13:07 +0100

    Add old html table

--- js/script.js
+++ js/script.js
@@ -25,6 +25,31 @@ const parseCsv = csv => {
   ]);
 }
 
+function setColor(data) {
+  if (data)
+      if (parseFloat(data) > 0)
+          return "<span class='positive'>" + data + "€<span>"
+      else return "<span class='negative'>" + data + "€<span>"
+}
+
+CsvToHtmlTable.init({
+  csv_path: "data/norden_social.csv",
+  element: "table-container",
+  allow_download: false,
+  csv_options: {
+      separator: ";",
+      delimiter: '"'
+  },
+  datatables_options: {
+      paging: true,
+      order: [[ 0, "desc" ]],
+      searching: false
+  },
+  custom_formatting: [
+      [2, setColor]
+  ]
+});
+
 /**
  * Group data by month and by transaction type like this:
  *
